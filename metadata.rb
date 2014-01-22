67fc279fcdaa31422d076120a5fc66323159af16
Hugo Correia <hugo.correia@klarna.com>
2014-01-22 11:24:58 +0100

    [CHEF-4979] add support for centos and multiple netwrok interfaces

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs/Configures drbd."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.9.0"
+version           "0.8.3"
 
-%w{ debian ubuntu }.each do |os|
+%w{ debian ubuntu centos }.each do |os|
   supports os
 end
